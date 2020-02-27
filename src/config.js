89d4d797f00a98d5fc206a0cae33a0329462f309
C1310861 Cristian Araujo De Jesus <c1310861@interno.bb.com.br>
2020-02-27 09:36:03 -0300

    Configurando repositório

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+  appID: process.env.REACT_APP_COMETCHAT_APP_ID,
+  apiKey: process.env.REACT_APP_COMETCHAT_APP_KEY
+};
+
+export default config;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { CometChat } from "@cometchat-pro/chat";
+import App from "./components/App";
+import config from "./config";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+CometChat.init(config.appID);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
